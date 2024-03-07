0bf18394ac294f8c4e360498bae034e735f2d10c
Sergey Mlayshev <malyshev.sa@phystech.edu>
2024-03-08 00:20:40 +0300

    Core realization

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,8 +1,12 @@
 #include "mainwindow.h"
-
+#include "main_vocabulary.h"
+#include "current_learning.h".h"
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    Main_vocabulary ob;
+    Current_learning c(ob);
+    c.choose_words_for_learning();
 }
 
 MainWindow::~MainWindow()
